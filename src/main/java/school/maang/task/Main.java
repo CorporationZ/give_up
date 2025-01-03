9e51af47ce68ed68a4f80e4cf010925597fb3480
Muhammadsolih <salikhdev@gmail.com>
2025-01-03 16:36:39 +0500

    Task1 success

---
+++ src/main/java/school/maang/task/Main.java
@@ -0,0 +1,11 @@
+package school.maang.task;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
+        System.out.println(n);
+
+    }
+}
