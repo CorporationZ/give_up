0af9195c482a04e73f14c85dd871869d34fd4c78
Muhammadsolih <salikhdev@gmail.com>
2025-01-04 19:06:13 +0500

    Create User. Update

--- src/main/java/school/maang/usercreate/Main.java
+++ src/main/java/school/maang/usercreate/Main.java
@@ -3,7 +3,7 @@ package school.maang.usercreate;
 public class Main {
     public static void main(String[] args) {
 
-        User user = new User("Solih" , 19 , "Google" , "London");
+        User user = new User("Solih", 19, "Google", "London");
 
         System.out.println(user.getJob());
         System.out.println(user.getName());
