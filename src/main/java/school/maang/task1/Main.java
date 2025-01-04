7e028f36bae76b1bfb42e291a24fe4e9f3a9e666
abrorhoja <abrorkocha0801@gmail.com>
2025-01-04 23:11:58 +0500

    task1 done

--- src/main/java/school/maang/task1/Main.java
+++ src/main/java/school/maang/task1/Main.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan =new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
     }
 }
