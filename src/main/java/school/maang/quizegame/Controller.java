a07a0d9ddb3ab4df4dae62528ff6225ba8fda469
Laziz582 <rajabboyevlaziz6@gmail.com>
2025-01-09 17:11:42 +0500

    Start Quize game.

---
+++ src/main/java/school/maang/quizegame/Controller.java
@@ -0,0 +1,4 @@
+package school.maang.quizegame;
+
+public class Controller {
+}
---
+++ src/main/java/school/maang/quizegame/Main.java
@@ -0,0 +1,10 @@
+package school.maang.quizegame;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+
+
+    }
+
+}
