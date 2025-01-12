111f86d119be0bfc8895f5cf90d99a8e8cef4ae5
Jamol <jamolmatqurbonov2005@gmail.com>
2025-01-12 16:59:32 +0500

    mathbot1

--- src/main/java/school/maang/Main.java
+++ src/main/java/school/maang/Main.java
@@ -1,7 +1,32 @@
 package school.maang;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import school.maang.telegrambot.MyBot;
+@Slf4j
 public class Main {
 
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new MyBot());
+            log.info("ishladi");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("ishlamadi");
+
+        }
+
+    }
+
+
 }
+
--- src/main/java/school/maang/telegrambot/MyBot.java
+++ src/main/java/school/maang/telegrambot/MyBot.java
@@ -1,4 +1,21 @@
 package school.maang.telegrambot;
 
-public class MyBot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class MyBot extends TelegramLongPollingBot {
+    @Override
+    public  String getBotToken(){
+        return "7628712941:AAGq4cyKnm2T-kULLsX_5BmZbIR9I7fJR2I";
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return "https://t.me/Math_uzb_misolbot";
+    }
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
 }
